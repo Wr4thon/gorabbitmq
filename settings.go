cc5915a0071bb6140d3f2882acc760d626b5c28b
Johannes Kolata <jko@fino.digital>
2019-05-24 16:10:30 +0200

    added json names to configuration

--- settings.go
+++ settings.go
@@ -2,17 +2,17 @@ package gorabbitmq
 
 // ConnectionSettings holds settings for a rabbitMQConnector connection
 type ConnectionSettings struct {
-	UserName string
-	Password string
-	Host     string
-	Port     int
+	UserName string `json:user_name`
+	Password string `json:password`
+	Host     string `json:host`
+	Port     int    `json:port`
 }
 
 // QueueSettings holds the configurationf for a Channel
 type QueueSettings struct {
-	QueueName        string
-	Durable          bool
-	Exclusive        bool
-	DeleteWhenUnused bool
-	NoWait           bool
+	QueueName        string `json:queue_name`
+	Durable          bool   `json:durable`
+	Exclusive        bool   `json:exclusive`
+	DeleteWhenUnused bool   `json:delete_when_unused`
+	NoWait           bool   `json:no_wait`
 }
