786f590645d0033e7fa0213fb6d8ae7f27058cbd
Wr4thon <jokolata@live.de>
2020-10-08 17:24:44 +0200

    documentation

--- middleware/errorcounter.go
+++ middleware/errorcounter.go
@@ -2,25 +2,29 @@ package middleware
 
 import "github.com/Wr4thon/gorabbitmq/v2"
 
-type errorCounter struct{}
+// ErrorCounter can be used to extract the ErrorCounter from the context.
+type ErrorCounter struct{}
 
 const errorCounterKey = "@errorCounter"
 
+// ErrorCounterConfig configures the ErrorCounter Middleware.
 // TODO validate
 type ErrorCounterConfig struct {
 	MaxRetries         int32
 	MaxRetriesExceeded func(gorabbitmq.Context)
 }
 
+// ErrorCounterWithConfig is a middleware that counts errors when they occur.
+// When they exceed a configurable Threshold a custom callback gets called.
 func ErrorCounterWithConfig(config ErrorCounterConfig) gorabbitmq.MiddlewareFunc {
 	return func(hf gorabbitmq.HandlerFunc) gorabbitmq.HandlerFunc {
 		return func(c gorabbitmq.Context) error {
 			if v, ok := c.Delivery().Headers[errorCounterKey]; ok {
 				if i, ok := v.(int32); ok {
-					c.Set(errorCounter{}, i)
+					c.Set(ErrorCounter{}, i)
 				}
 			} else {
-				c.Set(errorCounter{}, int32(0))
+				c.Set(ErrorCounter{}, int32(0))
 			}
 
 			err := hf(c)
@@ -32,7 +36,7 @@ func ErrorCounterWithConfig(config ErrorCounterConfig) gorabbitmq.MiddlewareFunc
 					table = make(map[string]interface{})
 				}
 
-				if errorCounter, ok := c.Value(errorCounter{}).(int32); ok {
+				if errorCounter, ok := c.Value(ErrorCounter{}).(int32); ok {
 					if errorCounter >= config.MaxRetries {
 						config.MaxRetriesExceeded(c)
 						c.Nack(false, false)
