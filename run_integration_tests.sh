93d582de55266bec62ad72bf6a742b7282c98eaa
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2023-08-17 11:46:11 +0200

    ci: lint and run integration tests
    Co-authored-by: Nicholai Gerschewsky <Nicholai.Gerschewsky@hotmail.com>

---
+++ run_integration_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+docker-compose down
+docker-compose up -d
+
+
+CONTAINER=rabbitmq
+
+echo "Checking service availability for $CONTAINER (CTRL+C to exit)"
+
+while [ "$(docker inspect -f {{.State.Health.Status}} $CONTAINER)" != "healthy" ]; do
+    sleep 1;
+done
+
+echo "Service $CONTAINER is now available"
+
+go test -run "Test_Integration" -vet=off -failfast -race -coverprofile=coverage.out
+go_test_exit_code=$?
+
+docker-compose down
+
+exit $go_test_exit_code
